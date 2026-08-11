71ceccee653a4b6da84d42787372cab3b2c3caa8
Marcelo Teixeira <marceloat01@gmail.com>
2026-08-11 22:01:07 +0100

    add photo

--- app.py
+++ app.py
@@ -1,17 +1,14 @@
+from PIL import Image
 import platform
 
-from renderer import render_message
-
-image = render_message("Hello Aryana")
+image = Image.open("photo.jpeg")
+image = image.convert("RGB")
+image = image.resize((600, 400))
 
 if platform.system() == "Darwin":
-    # Mac development
     image.show()
-
 else:
-    # Raspberry Pi
     from inky.auto import auto
-
     inky = auto()
     inky.set_image(image)
     inky.show()
