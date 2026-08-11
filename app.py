4bfed156c15fc23d28bc8df7d35d3c229e3f51c5
Marcelo Teixeira <marceloat01@gmail.com>
2026-08-11 21:48:51 +0100

    app.py for both mac an pi

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
+import platform
+
 from renderer import render_message
 
 image = render_message("Hello Aryana")
 
-image.save("preview.png")
-image.show()
+if platform.system() == "Darwin":
+    # Mac development
+    image.show()
+
+else:
+    # Raspberry Pi
+    from inky.auto import auto
+
+    inky = auto()
+    inky.set_image(image)
+    inky.show()
