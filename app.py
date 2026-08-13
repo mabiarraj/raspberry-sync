c2c36bf5714b9a3640dfe4265d71d0890c51cc06
Marcelo Teixeira <marceloat01@gmail.com>
2026-08-13 21:19:45 +0100

    wip

--- app.py
+++ app.py
@@ -8,16 +8,22 @@ from renderer import render_message
 WIDTH = 600
 HEIGHT = 400
 
-image = render_message("Hello Aryana")
 
-if platform.system() == "Darwin":
+def get_image():
     img_dir = Path(__file__).parent / "img"
     images = [path for path in img_dir.iterdir() if path.is_file()] if img_dir.exists() else []
 
     if images:
         image = Image.open(images[0]).convert("RGB")
         image.thumbnail((WIDTH, HEIGHT), Image.Resampling.LANCZOS)
+        return image
 
+    return render_message("Hello Aryana")
+
+
+image = get_image()
+
+if platform.system() == "Darwin":
     image.show()
 else:
     from inky.auto import auto
