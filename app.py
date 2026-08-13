11f8ab2c39e0fecaa69a8abfb5b4c0b82f6a2a0d
Marcelo Teixeira <marceloat01@gmail.com>
2026-08-13 21:10:50 +0100

    Wip

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
 import platform
+from pathlib import Path
+
+from PIL import Image
 
 from renderer import render_message
+
+WIDTH = 600
+HEIGHT = 400
 
 image = render_message("Hello Aryana")
 
 if platform.system() == "Darwin":
+    img_dir = Path(__file__).parent / "img"
+    images = [path for path in img_dir.iterdir() if path.is_file()] if img_dir.exists() else []
+
+    if images:
+        image = Image.open(images[0]).convert("RGB")
+        image.thumbnail((WIDTH, HEIGHT), Image.Resampling.LANCZOS)
+
     image.show()
 else:
     from inky.auto import auto
